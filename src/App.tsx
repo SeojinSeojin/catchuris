3e8ca2aa74e516934acbe5df21be3122b637f07c
seojin kim <001106ksj@gmail.com>
2021-12-11 22:41:28 +0900

    chore: 초기 설정

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GameTable from './components/GameTable';
+
+function App() {
+  return (
+    <div className='App'>
+      <GameTable />
+    </div>
+  );
+}
+
+export default App;
