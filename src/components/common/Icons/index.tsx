f25f24823b47dc44d193ef2f48310d34805b5803
Seojin Kim <seojin.kim@voyagerx.com>
2023-02-18 02:06:00 +0900

    이미지 에셋 추가

--- src/components/common/Icons/index.tsx
+++ src/components/common/Icons/index.tsx
@@ -1,31 +1,39 @@
-import { ReactComponent as CatchuBlue } from '../../../assets/image/catchu/blue.svg';
-import { ReactComponent as CatchuGreen } from '../../../assets/image/catchu/green.svg';
-import { ReactComponent as CatchuOrange } from '../../../assets/image/catchu/orange.svg';
-import { ReactComponent as CatchuPurple } from '../../../assets/image/catchu/purple.svg';
-import { ReactComponent as CatchuSkyblue } from '../../../assets/image/catchu/skyblue.svg';
-import { ReactComponent as CatchuWhite } from '../../../assets/image/catchu/white.svg';
-import { ReactComponent as CatchuYellow } from '../../../assets/image/catchu/yellow.svg';
-import { ReactComponent as CatchuYellowGreen } from '../../../assets/image/catchu/yellowgreen.svg';
-import { ReactComponent as IcGameOver } from '../../../assets/image/game-over.svg';
-import { ReactComponent as IcPlay } from '../../../assets/icons/ic-play.svg';
-import { ReactComponent as IcRefresh } from '../../../assets/icons/ic-refresh.svg';
-import { ReactComponent as IcStop } from '../../../assets/icons/ic-stop.svg';
+import { ReactComponent as IcArrowDown } from '../../../assets/icons/ic-arrow-down.svg';
 import { ReactComponent as IcArrowLeft } from '../../../assets/icons/ic-arrow-left.svg';
 import { ReactComponent as IcArrowRight } from '../../../assets/icons/ic-arrow-right.svg';
-import { ReactComponent as IcArrowDown } from '../../../assets/icons/ic-arrow-down.svg';
 import { ReactComponent as IcDown } from '../../../assets/icons/ic-down.svg';
+import { ReactComponent as IcPlay } from '../../../assets/icons/ic-play.svg';
+import { ReactComponent as IcRefresh } from '../../../assets/icons/ic-refresh.svg';
 import { ReactComponent as IcRotate } from '../../../assets/icons/ic-rotate.svg';
 import { ReactComponent as IcSave } from '../../../assets/icons/ic-save.svg';
+import { ReactComponent as IcStop } from '../../../assets/icons/ic-stop.svg';
+import CatchuBlueSrc, {
+  ReactComponent as CatchuBlue,
+} from '../../../assets/image/catchu/blue.svg';
+import CatchuGreenSrc, {
+  ReactComponent as CatchuGreen,
+} from '../../../assets/image/catchu/green.svg';
+import CatchuOrangeSrc, {
+  ReactComponent as CatchuOrange,
+} from '../../../assets/image/catchu/orange.svg';
+import CatchuPurpleSrc, {
+  ReactComponent as CatchuPurple,
+} from '../../../assets/image/catchu/purple.svg';
+import CatchuSkyblueSrc, {
+  ReactComponent as CatchuSkyblue,
+} from '../../../assets/image/catchu/skyblue.svg';
+import CatchuWhiteSrc, {
+  ReactComponent as CatchuWhite,
+} from '../../../assets/image/catchu/white.svg';
+import CatchuYellowSrc, {
+  ReactComponent as CatchuYellow,
+} from '../../../assets/image/catchu/yellow.svg';
+import CatchuYellowGreenSrc, {
+  ReactComponent as CatchuYellowGreen,
+} from '../../../assets/image/catchu/yellowgreen.svg';
+import { ReactComponent as IcGameOver } from '../../../assets/image/game-over.svg';
 import { ReactComponent as ImageCatchues } from '../../../assets/image/image-catchues.svg';
 import { ReactComponent as ImageLogo } from '../../../assets/image/image-logo.svg';
-import CatchuBlueSrc from '../../../assets/image/catchu/blue.svg';
-import CatchuGreenSrc from '../../../assets/image/catchu/green.svg';
-import CatchuOrangeSrc from '../../../assets/image/catchu/orange.svg';
-import CatchuPurpleSrc from '../../../assets/image/catchu/purple.svg';
-import CatchuSkyblueSrc from '../../../assets/image/catchu/skyblue.svg';
-import CatchuWhiteSrc from '../../../assets/image/catchu/white.svg';
-import CatchuYellowSrc from '../../../assets/image/catchu/yellow.svg';
-import CatchuYellowGreenSrc from '../../../assets/image/catchu/yellowgreen.svg';
 
 const catchuBlueImage = new Image();
 catchuBlueImage.src = CatchuBlueSrc;
